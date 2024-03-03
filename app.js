5a528cbcb5b8a653cfeabcef76e1a5f0fb4ad747
Pranav Katiyar <pk.pranavkatiyar@gmail.com>
2024-03-03 12:56:58 +0530

    cors enabled

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
+import { CourierClient } from "@trycourier/courier";
 const express = require('express');
 const cors = require('./cors');
-
 const app = express();
 
 app.use(cors);
@@ -18,6 +18,25 @@ const employees = [
 app.get('/employees', (req, res) => {
     res.json(employees);
 });
+
+app.get('/send', async (req, res) => {
+    const courier = CourierClient({ authorizationToken: "pk_prod_BDF878SPXPMDR1KHA20DB23RZVYA" });
+    const { requestId } = await courier.send({
+        message: {
+            content: {
+                title: "Welcome to Courier!",
+                body: "Want to hear a joke?"
+            },
+            data: {
+                joke: "Why was the JavaScript developer sad? Because they didn't Node how to Express themselves"
+            },
+            to: {
+                email: "prnvkatiyar@gmail.com"
+            }
+        }
+    });
+    console.log(requestId);
+})
 
 // Start the server
 const port = process.env.PORT || 3000;
