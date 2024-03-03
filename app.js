76a144e5596dbe684ff49b9d22e1edc3e7513ced
Pranav Katiyar <pk.pranavkatiyar@gmail.com>
2024-03-03 13:17:08 +0530

    send sms 4

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import { CourierClient } from "@trycourier/courier";
 import express from 'express';
-import cors from './cors';
+import cors from 'cors';
 const app = express();
-app.use(cors);
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: 'GET',
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 const employees = [
     { id: 1, name: 'John Doe', department: 'Engineering' },
